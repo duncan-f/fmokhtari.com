bf4202223ffd37f5de0ce25631e973ddcf62e2a6
dunCan <f0uad.f@hotmail.com>
2023-02-20 12:20:46 +0300

    initial commit :rocket:

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  }), react()]
+});
