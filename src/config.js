0dfc536adb0579d3e61d219035cc04d24f93724a
dunCan <f0uad.f@hotmail.com>
2023-02-22 12:51:14 +0300

    added new components :man_technologist:

---
+++ src/config.js
@@ -0,0 +1,5 @@
+export const SITE_URL = '/'
+export const SITE_NAME = 'Farouk Mokhtari'
+export const SITE_AUTHOR = 'Farouk Mokhtari'
+export const SITE_DESCRIPTION = 'This is my personal website. Here, I share my knowledge of technology.'
+
