4e83c2156f063a780d76b35f641538e8e21f55eb
dunCan <f0uad.f@hotmail.com>
2023-02-28 13:08:13 +0300

    :man_technologist: added hamburger menu

--- src/components/Hamburger.tsx
+++ src/components/Hamburger.tsx
@@ -1,8 +1,11 @@
+import { Menu, Transition } from '@headlessui/react';
+import React, { Fragment } from 'react';
+
 export default function Hamburger() {
   return (
-    <div className="md:hidden relative inline-block text-left">
-      <button className="relative group">
-        <div className="relative flex overflow-hidden items-center justify-center rounded-lg w-[40px] h-[40px] transform transition-all bg-slate-700 ring-0 ring-gray-300 hover:ring-8 group-focus:ring-4 ring-opacity-30 duration-200 shadow-md">
+    <Menu as="div" className="md:hidden relative inline-block text-left">
+      <Menu.Button className="relative group">
+        <div className="relative flex overflow-hidden items-center justify-center rounded-lg w-[40px] h-[40px] transform transition-all bg-zinc-800 dark:bg-zinc-700 ring-0 ring-gray-300 hover:ring-8 group-focus:ring-4 ring-opacity-30 duration-200 shadow-md">
           <div className="flex flex-col justify-between w-[20px] h-[20px] transform transition-all duration-300 origin-center overflow-hidden">
             <div className="bg-white h-[2px] w-7 transform transition-all duration-300 origin-left group-focus:translate-x-10"></div>
             <div className="bg-white h-[2px] w-7 rounded transform transition-all duration-300 group-focus:translate-x-10 delay-75"></div>
@@ -14,27 +17,38 @@ export default function Hamburger() {
             </div>
           </div>
         </div>
-      </button>
-      <div id="dropdown" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
-        <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="hamburger">
-          <li>
+      </Menu.Button>
+
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+      <Menu.Items className="absolute right-0 origin-top-right top-14 z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-zinc-700">
+        <div className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="hamburger">
+          <Menu.Item>
             <a
               href="/projects"
-              className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+              className="block px-4 py-2 hover:bg-zinc-100 dark:hover:bg-zinc-600 dark:hover:text-white"
             >
               Projects
             </a>
-          </li>
-          <li>
+          </Menu.Item>
+          <Menu.Item>
             <a
               href="/posts"
-              className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+              className="block px-4 py-2 hover:bg-zinc-100 dark:hover:bg-zinc-600 dark:hover:text-white"
             >
               Blog
             </a>
-          </li>
-        </ul>
-      </div>
-    </div>
+          </Menu.Item>
+        </div>
+      </Menu.Items>
+      </Transition>
+    </Menu>
   );
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,11 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   darkMode: 'class',
 	theme: {
+    safelist: [
+      'animate-[fade-in_1s_ease-in-out]',
+      'animate-[fade-in-up_1s_ease-in-out]',
+      'animate-[slide-out-left_1s_ease-in-out]'
+    ],
 		extend: {
       fontFamily: {
         rubik: ["'Rubik'", 'Verdana', 'sans-serif']
