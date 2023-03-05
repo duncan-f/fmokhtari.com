0179b45edbbfc34f1b44a725f554a53864fb3283
dunCan <f0uad.f@hotmail.com>
2023-03-05 16:36:59 +0300

    :sunglasses: changed color theme

--- src/components/Hamburger.tsx
+++ src/components/Hamburger.tsx
@@ -25,7 +25,7 @@ export default function Hamburger() {
           <Menu.Item>
             <a
               href="/projects"
-              className="block px-4 py-2 hover:bg-orange-100 dark:hover:bg-zinc-600 dark:hover:text-white"
+              className="block px-4 py-2 hover:bg-teal-100 dark:hover:bg-zinc-600 dark:hover:text-white"
             >
               Projects
             </a>
@@ -33,7 +33,7 @@ export default function Hamburger() {
           <Menu.Item>
             <a
               href="/posts"
-              className="block px-4 py-2 hover:bg-orange-100 dark:hover:bg-zinc-600 dark:hover:text-white"
+              className="block px-4 py-2 hover:bg-teal-100 dark:hover:bg-zinc-600 dark:hover:text-white"
             >
               Blog
             </a>
